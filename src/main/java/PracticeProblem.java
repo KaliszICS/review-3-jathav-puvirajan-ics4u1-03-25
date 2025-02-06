407ccae3117abf34f3be2446ce44329876e6cb57
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-02-06 22:03:26 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,37 +13,69 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String string = in.nextLine();
+		System.out.println(string.charAt(0));
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		boolean bool = in.nextBoolean();
+		in.nextLine();
+		System.out.println(!bool);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int n = in.nextInt();
+		in.nextLine();
+		System.out.println(n > 5);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double n = in.nextDouble();
+		in.nextLine();
+		System.out.println(n >= -2.0 && n <= 2.0);
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String string = in.nextLine();
+		System.out.println(string.equals("Hello World"));
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int n1 = in.nextInt();
+		in.nextLine();
+		System.out.print("In: ");
+		int n2 = in.nextInt();
+		in.nextLine();
+		System.out.println(n1 <= n2);
 		
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double n1 = in.nextDouble();
+		in.nextLine();
+		System.out.print("In: ");
+		double n2 = in.nextDouble();
+		in.nextLine();
+		System.out.println(n1 > n2);
 		
 	}
 
